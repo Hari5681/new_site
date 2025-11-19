84eb70b4ab339855b0543c2ef7ef52d1d3a527cf
Hari Krishna <rowdylucky888@gmail.com>
2025-11-19 11:52:23 +0000

    Cherished Memories    delete this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { WelcomeSection } from "@/components/sections/welcome-section";
 import { TimelineSection } from "@/components/sections/timeline-section";
-import { GallerySection } from "@/components/sections/gallery-section";
 import { LetterSection } from "@/components/sections/letter-section";
 import { EndingSection } from "@/components/sections/ending-section";
 
@@ -9,7 +8,6 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <WelcomeSection />
       <TimelineSection />
-      <GallerySection />
       <LetterSection />
       <EndingSection />
     </main>
